7db4a412d9cd1003d09d3fbcece7ee7495d728c5
arnie <arniebulawan@gmail.com>
2021-04-27 04:03:55 +0800

    add sample users route to api middleware

--- routes/api.php
+++ routes/api.php
@@ -15,7 +15,25 @@ use Illuminate\Http\Request;
 
 // Route::post('register', 'UserController@register');
 // users route
-Route::prefix('users')
+// Route::prefix('users')
+//     ->group(function () {
+//         Route::get('/', function () {
+//             $response = array (
+//                 'user' => array (
+//                     'first_name' => 'John',
+//                     'last_name' => 'Doe',
+//                     'email' => 'johndoe@gmail.com'
+//                 ),
+//                 'code' => 200,
+//             );
+//             return response()->json($response, $response['code']);
+//         });
+//     });
+
+Route::group(['middleware' => ['auth:api']], function () {
+
+    // users route
+    Route::prefix('users')
     ->group(function () {
         Route::get('/', function () {
             $response = array (
@@ -29,23 +47,5 @@ Route::prefix('users')
             return response()->json($response, $response['code']);
         });
     });
-
-Route::group(['middleware' => ['auth:api']], function () {
-
-    // // users route
-    // Route::prefix('users')
-    //     ->group(function () {
-    //         Route::get('/', function () {
-    //             $this->response['data'] = [
-    //                 'first_name' => 'John',
-    //                 'last_name' => 'Doe',
-    //                 'email' => 'johndoe@gmail.com'
-    //             ];
-    //             $this->response = [
-    //                 'code' => 200,
-    //             ];
-    //             return response()->json($this->response, $this->response['code']);
-    //         });
-    //     });
 
 });
